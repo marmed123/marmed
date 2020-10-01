0f52f6ee819f4a0cc168956058875c0db2c253b4
marwenkhorchani <marwenkhorchani7@gmail.com>
2020-10-01 18:01:24 +0100

    second commit

--- marmed.js
+++ marmed.js
@@ -1,24 +1,32 @@
 var login=[
 {
+	name: "naouress",
+	login:"naouress",
+	password:"best group"
+},
+{
 	name: "marwen.khorchani",
 	login:"marwen",
-	password:123456
+	password:"123456"
 },
 {   name: "marwen.khorchani",
 	login:"ahmed",
-	password:123456789
+	password:"123456789"
 },
 {
 	name: "marwen.khorchani",
 	login:"heni",
-	password:111111
+	password:"111111"
 },
 {
 	name: "marwen.khorchani",
 	login:"nouha",
-	password:111111111
+	password:"111111111"
 }
 ]
+
+// $("account").hide()
+
 
 var name=""
 $("#c").click(function(){
@@ -27,7 +35,7 @@ $("#c").click(function(){
 	}else{
 		var counter=0
 		for(var i=0;i<login.length;i++){
-			if(login[i]["login"] === $("#a").val() && login[i]["password"] === parseInt($("#b").val())){
+			if(login[i]["login"] === $("#a").val() && login[i]["password"] === $("#b").val()){
 				counter++
 				name=login[i]["name"]
 			}
@@ -41,7 +49,16 @@ $("#c").click(function(){
 			// window.alert("acceuil.")
 			// window.close()
 			// window.open("marmed1.html")
-			$("# loginBloc").hide()
+			$("#loginBloc").hide() 
+			$("#account").show()	
+			$('body').css({
+			 "margin":0, "padding":0, "font-family":"Roboto, sans-serif","background-image": "url('https://cdn.nohat.cc/thumb/f/720/fb27363b3e7649db9aa2.jpg')"
+			 ,"background-repeat":"no-repeat"
+			})
+		
+
+			
+
 		}
 	}
 })
